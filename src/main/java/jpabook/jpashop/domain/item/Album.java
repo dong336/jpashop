e9a37d7dd2e73afc33c2c1b224dadd692c701562
kdw <50583568+dong336@users.noreply.github.com>
2023-11-14 21:30:20 +0900

    init

---
+++ src/main/java/jpabook/jpashop/domain/item/Album.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.domain.item;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+import lombok.Getter;
+
+@Entity
+@DiscriminatorValue("A")
+@Getter
+public class Album extends Item{
+    
+    private String artist;
+    private String etc;
+}
---
+++ src/main/java/jpabook/jpashop/domain/item/Movie.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.domain.item;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+import lombok.Getter;
+
+@Entity
+@DiscriminatorValue("M")
+@Getter
+public class Movie extends Item{
+    
+    private String director;
+    private String actor;
+}
